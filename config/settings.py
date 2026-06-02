8da5d3a02ed08619b4bea1cd6bd107a71db52a2e
Fallin1 <balogunmotolani16@gmail.com>
2026-06-02 19:55:37 +0100

    Use forgiving static storage on Render

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,7 @@ STORAGES = {
         'BACKEND': (
             'django.contrib.staticfiles.storage.StaticFilesStorage'
             if DEBUG
-            else 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+            else 'whitenoise.storage.CompressedStaticFilesStorage'
         ),
     },
 }
